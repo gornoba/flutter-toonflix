6af52c6ddcdb67b8e6d979a5d106758781359b25
HyunHoo Im <litinglabs03@gmail.com>
2023-02-14 20:38:37 +0900

    widget lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTile = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTile = !showTile;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,8 +37,14 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
+            children: [
+              showTile ? const MyLargeTitle() : const Text('Nothing to see'),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(
+                  Icons.remove_red_eye,
+                ),
+              )
             ],
           ),
         ),
@@ -39,13 +53,31 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print('initState!');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('buld');
     return Text(
       'My Large Title',
       style: TextStyle(
