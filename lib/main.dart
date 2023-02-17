976f108bdf53ea5288ed315935dee8cc427760d0
HyunHoo Im <litinglabs03@gmail.com>
2023-02-17 20:28:11 +0900

    timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,232 +1,25 @@
 import 'package:flutter/material.dart';
-// import 'package:toonflix/widgets/button.dart';
-// import 'package:toonflix/widgets/currency_card.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTile = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTile = !showTile;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              color: Color(0xFF232B55),
+            ),
           ),
-        ),
-      ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTile ? const MyLargeTitle() : const Text('Nothing to see'),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: const Icon(
-                  Icons.remove_red_eye,
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+          cardColor: const Color(0xFFF4EDDB),
+          scaffoldBackgroundColor: const Color(0xFFE7626C)),
+      home: const HomeScreen(),
     );
   }
 }
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print('initState!');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print("dispose");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('buld');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
-    );
-  }
-}
-
-// class App extends StatelessWidget {
-//   const App({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: const Color(0xFF181818),
-//         body: SingleChildScrollView(
-//           child: Padding(
-//             padding: const EdgeInsets.symmetric(
-//               horizontal: 20,
-//             ),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 const SizedBox(
-//                   height: 80,
-//                 ),
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.end,
-//                   children: [
-//                     Column(
-//                       crossAxisAlignment: CrossAxisAlignment.end,
-//                       children: [
-//                         const Text(
-//                           'Hey, Selena',
-//                           style: TextStyle(
-//                             color: Colors.white,
-//                             fontSize: 28,
-//                             fontWeight: FontWeight.w800,
-//                           ),
-//                         ),
-//                         Text(
-//                           'Welcome back',
-//                           style: TextStyle(
-//                             color: Colors.white.withOpacity(0.8),
-//                             fontSize: 18,
-//                           ),
-//                         ),
-//                       ],
-//                     )
-//                   ],
-//                 ),
-//                 const SizedBox(
-//                   height: 70,
-//                 ),
-//                 Text(
-//                   'Total Balacne',
-//                   style: TextStyle(
-//                     fontSize: 22,
-//                     color: Colors.white.withOpacity(0.8),
-//                   ),
-//                 ),
-//                 const SizedBox(
-//                   height: 10,
-//                 ),
-//                 const Text(
-//                   '\$5 194 482',
-//                   style: TextStyle(
-//                     fontSize: 48,
-//                     color: Colors.white,
-//                     fontWeight: FontWeight.w600,
-//                   ),
-//                 ),
-//                 const SizedBox(
-//                   height: 30,
-//                 ),
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: const [
-//                     Button(
-//                       text: 'Transfer',
-//                       bgColor: Color(0xFFF2B33A),
-//                       textColor: Colors.black,
-//                     ),
-//                     Button(
-//                       text: 'Request',
-//                       bgColor: Color(0xFF1F2123),
-//                       textColor: Colors.white,
-//                     ),
-//                   ],
-//                 ),
-//                 const SizedBox(
-//                   height: 50,
-//                 ),
-//                 Row(
-//                   crossAxisAlignment: CrossAxisAlignment.center,
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     const Text(
-//                       'Wallets',
-//                       style: TextStyle(
-//                         color: Colors.white,
-//                         fontSize: 36,
-//                         fontWeight: FontWeight.w600,
-//                       ),
-//                     ),
-//                     Text(
-//                       'View All',
-//                       style: TextStyle(
-//                         color: Colors.white.withOpacity(0.8),
-//                         fontSize: 18,
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//                 const SizedBox(
-//                   height: 20,
-//                 ),
-//                 const CurrencyCard(
-//                   name: 'Euro',
-//                   code: 'EUR',
-//                   amount: '6,428',
-//                   icon: Icons.euro_outlined,
-//                   isInverted: false,
-//                   offsety: 0,
-//                 ),
-//                 const CurrencyCard(
-//                   name: 'Bitcoin',
-//                   code: 'BTC',
-//                   amount: '9,275',
-//                   icon: Icons.currency_bitcoin,
-//                   isInverted: true,
-//                   offsety: 1,
-//                 ),
-//                 const CurrencyCard(
-//                   name: 'Dollar',
-//                   code: 'USD',
-//                   amount: '428',
-//                   icon: Icons.attach_money_outlined,
-//                   isInverted: false,
-//                   offsety: 2,
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
